ad9bfc63b84d126eff2dca817464568ca9b820b7
thewizardplusplus <thewizardplusplus@yandex.ru>
2013-10-26 00:18:22 +0400

    Small correct of behavior.

--- js/diary.js
+++ js/diary.js
@@ -2,10 +2,7 @@ function processPointStateChoise(id, state) {
 	jQuery('#point_list').yiiGridView('update', {
 		type: 'POST',
 		url: '?r=point/update&id=' + id,
-		data: {
-			'Point[state]': state,
-			'Point[check]': state != 'INITIAL' ? 1 : 0
-		},
+		data: { 'Point[state]': state },
 		success: function(data) {
 			jQuery('#point_list').yiiGridView('update');
 		}
