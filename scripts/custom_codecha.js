20087383d44f119425610d4333be4fc8f39b9adf
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-06-08 22:18:24 +0400

    Correct bug with login form reaction on Enter key; correct styles of codecha.

--- scripts/custom_codecha.js
+++ scripts/custom_codecha.js
@@ -1,5 +1,6 @@
 $(document).ready(
 	function() {
+		var ENTER_KEY_CODE = 13;
 		var RequestAnimationFrame =
 			requestAnimationFrame
 			|| mozRequestAnimationFrame
@@ -7,36 +8,44 @@ $(document).ready(
 			|| function(callback) {
 				setTimeout(callback, 1000 / 60);
 			};
+		var login_form = $('#login-form');
 		var WaitVerification = function() {
 			if ($('#codecha_code_submit_button').length) {
 				RequestAnimationFrame(WaitVerification);
 			} else {
-				$('.login-button').click();
+				login_form.submit();
 			}
 		};
-		var SendCode = function() {
+		var SubmitCode = function() {
 			var codecha_submit_button = $('#codecha_code_submit_button');
 			if (codecha_submit_button.length) {
 				codecha_submit_button.click();
 				WaitVerification();
-
-				return false;
-			} else {
-				return true;
 			}
 		};
 
-		$('#codecha_code_area').on(
+		$('#codecha_bottom #codecha_change_challenge').remove();
+		$('#codecha_bottom #codecha_language_selector').remove();
+
+		$('.login-button').click(
+			function() {
+				SubmitCode();
+				return false;
+			}
+		);
+		login_form.on(
 			'keypress',
 			function(event) {
-				// 13 - Enter key
-				if (event.keyCode == 13 && event.ctrlKey) {
-					SendCode();
+				var key_code = event.keyCode || event.which;
+				if (key_code == ENTER_KEY_CODE) {
+					var source = event.target || event.srcElement;
+					if (source != $('#codecha_code_area').get(0)) {
+						event.preventDefault();
+					} else if (event.ctrlKey) {
+						SubmitCode();
+					}
 				}
 			}
 		);
-		$('.login-button').click(SendCode);
-
-		$('#codecha_bottom :not(#codecha_code_submit_button)').remove();
 	}
 );
