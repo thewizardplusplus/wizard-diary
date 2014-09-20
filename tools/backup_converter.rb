1bbd12086f4416a55e92dfea0ca6969a51b53975
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-09-20 22:51:49 +0400

    Fixes #18: add point text escaping in new backup converter.

--- tools/backup_converter.rb
+++ tools/backup_converter.rb
@@ -1,28 +1,32 @@
 #!/usr/bin/env ruby
 
 require 'rexml/document'
+require 'mysql2'
 
 begin
-    raise "need to specify a file to process" unless ARGV.length > 0
-    backup_filename = ARGV.first
+	raise "need to specify a file to process" unless ARGV.length > 0
+	backup_filename = ARGV.first
 
-    backup_file = File.new(backup_filename)
-    xml = REXML::Document.new(backup_file)
-    xml.elements.each('diary/day') do |day|
-        date = day.attributes['date']
-        order = 1
-        day.elements.each('point') do |point|
-            p date
-            p point.attributes['state']
-            p point.attributes['check'] ? 1 : 0
-            p point.cdatas().join('')
-            p order
-            p ''
+	backup_file = File.new(backup_filename)
+	xml = REXML::Document.new(backup_file)
+	xml.elements.each('diary/day') do |day|
+		date = day.attributes['date']
+		order = 1
+		day.elements.each('point') do |point|
+			raw_text = point.cdatas().join('')
+			escaped_text = Mysql2::Client.escape(raw_text)
 
-            order += 2
-        end
-        p ''
-    end
+			p date
+			p point.attributes['state']
+			p point.attributes['check'] ? 1 : 0
+			p escaped_text
+			p order
+			p ''
+
+			order += 2
+		end
+		p ''
+	end
 rescue Exception => exception
-    puts "Error: \"#{exception.message}\"."
+	puts "Error: \"#{exception.message}\"."
 end
