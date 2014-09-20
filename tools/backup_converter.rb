9552b3cafde2c82e313d893fbcc98d46d04a4e6a
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-09-20 23:17:29 +0400

    Fixes #18: add SQL script formatting in new backup converter.

--- tools/backup_converter.rb
+++ tools/backup_converter.rb
@@ -7,25 +7,35 @@ begin
 	raise "need to specify a file to process" unless ARGV.length > 0
 	backup_filename = ARGV.first
 
+	puts 'DELETE FROM `diary_points`;'
+	puts 'INSERT INTO `diary_points` ' +
+		'(`date`, `text`, `state`, `check`, `order`)'
+	puts 'VALUES'
+
 	backup_file = File.new(backup_filename)
 	xml = REXML::Document.new(backup_file)
 	xml.elements.each('diary/day') do |day|
 		date = day.attributes['date']
 		order = 1
 		day.elements.each('point') do |point|
+			state = point.attributes['state']
+			check = point.attributes['check'] ? 1 : 0
+
 			raw_text = point.cdatas().join('')
 			escaped_text = Mysql2::Client.escape(raw_text)
 
-			p date
-			p point.attributes['state']
-			p point.attributes['check'] ? 1 : 0
-			p escaped_text
-			p order
-			p ''
+			separator = day.next_element() || point.next_element() ? ',' : ';'
+
+			puts "\t(" +
+				"'#{date}', " +
+				"'#{escaped_text}', " +
+				"'#{state}', " +
+				"#{check}, " +
+				"#{order}" +
+			")#{separator}"
 
 			order += 2
 		end
-		p ''
 	end
 rescue Exception => exception
 	puts "Error: \"#{exception.message}\"."
