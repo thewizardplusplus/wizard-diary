d830dc30f321c8270dec59d38e0c12a5b42f27a4
thewizardplusplus <thewizardplusplus@yandex.ru>
2014-09-20 22:17:41 +0400

    Fixes #18: add file reading and XML document processing in new backup converter.

--- tools/backup_converter.rb
+++ tools/backup_converter.rb
@@ -1,11 +1,28 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
+require 'rexml/document'
 
 begin
     raise "need to specify a file to process" unless ARGV.length > 0
     backup_filename = ARGV.first
-    p backup_filename
+
+    backup_file = File.new(backup_filename)
+    xml = REXML::Document.new(backup_file)
+    xml.elements.each('diary/day') do |day|
+        date = day.attributes['date']
+        order = 1
+        day.elements.each('point') do |point|
+            p date
+            p point.attributes['state']
+            p point.attributes['check'] ? 1 : 0
+            p point.cdatas().join('')
+            p order
+            p ''
+
+            order += 2
+        end
+        p ''
+    end
 rescue Exception => exception
     puts "Error: \"#{exception.message}\"."
 end
