baff38a07ca93e3b2bcde76a463c9503876dabf4
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-01-12 04:32:24 +0300

    Fixes #24: remove debug output.

--- protected/commands/ClearAccessLogCommand.php
+++ protected/commands/ClearAccessLogCommand.php
@@ -2,7 +2,6 @@
 
 class ClearAccessLogCommand extends CConsoleCommand {
 	public function run() {
-		Yii::log(Parameters::getModel()->access_log_lifetime_in_s);
 		Access::model()->deleteAll(
 			'timestamp < SUBDATE(NOW(), INTERVAL '
 			. Parameters::getModel()->access_log_lifetime_in_s
