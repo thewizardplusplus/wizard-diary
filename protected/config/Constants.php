ac8bf4e02b42edd804cb49b05aad0c307921a4b4
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-04-15 22:52:21 +0300

    Change default mode of sending access code.

--- protected/config/Constants.php
+++ protected/config/Constants.php
@@ -21,7 +21,7 @@ class Constants extends AccessConstants {
 	const DROPBOX_REDIRECT_URL = '/backup/redirect';
 	const ACCESS_CODE_LENGTH = 5;
 	const ACCESS_CODE_LIFETIME_IN_S = 60;
-	const ACCESS_CODE_SEND_EMAIL = true;
+	const ACCESS_CODE_SEND_EMAIL = false;
 	// 30 days
 	const ACCESS_LOG_LIFETIME_IN_S_DEFAULT = 2592000;
 	const ACCESS_LOG_UPDATE_PAUSE_IN_S = 30;
