108bac8aea110768099ef8e47cf6502e0ae178fb
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-06-08 04:08:49 +0300

    Fixes #83: correct processing of daily points adding.

--- protected/commands/AddDailyPointsCommand.php
+++ protected/commands/AddDailyPointsCommand.php
@@ -2,17 +2,6 @@
 
 class AddDailyPointsCommand extends CConsoleCommand {
 	public function run() {
-		Yii::app()
-			->db
-			->createCommand(
-				'INSERT INTO {{points}} (date, text, `check`, daily) '
-				. 'SELECT CURDATE(), text, `check`, TRUE '
-				. 'FROM {{daily_points}} '
-				. 'ORDER BY `order`'
-			)
-			->execute();
-
-		$date = date('Y-m-d');
-		Point::renumberOrderFieldsForDate($date);
+		DailyPointsAdder::addDailyPoints();
 	}
 }
---
+++ protected/components/DailyPointsAdder.php
@@ -0,0 +1,18 @@
+<?php
+
+class DailyPointsAdder {
+	public static function addDailyPoints() {
+		Yii::app()
+			->db
+			->createCommand(
+				'INSERT INTO {{points}} (date, text, `check`, daily) '
+				. 'SELECT CURDATE(), text, `check`, TRUE '
+				. 'FROM {{daily_points}} '
+				. 'ORDER BY `order`'
+			)
+			->execute();
+
+		$date = date('Y-m-d');
+		Point::renumberOrderFieldsForDate($date);
+	}
+}
