69ee74692bf71013b14d43170276c05c4936593e
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-09-21 10:01:20 +0300

    Issue #106: correct permissions corrector.

--- tools/permissions_corrector.sh
+++ tools/permissions_corrector.sh
@@ -4,22 +4,27 @@ function ShowHelp() {
 	local -r script_name=`basename $0`
 
 	echo "Using:"
-	echo -e "\t$script_name <command> <path>"
+	echo -e "\t$script_name -h | --help"
+	echo -e "\t$script_name lock <path>"
+	echo -e "\t$script_name unlock <path>"
+	echo -e "\t$script_name list <path>"
+	echo ""
+	echo "Options:"
+	echo -e "\t-h, --help  - show help."
 	echo ""
 	echo "Commands:"
-	echo -e "\tlock - set permissions" \
-		"0444 to all files" \
-		"and 0555 to all directories" \
-		"in directory on <path>;"
-	echo -e "\tunlock - set permissions" \
-		"0777 to all files and directories" \
-		"in directory on <path>."
+	echo -e "\tlock <path>    - set permissions 0444 to all files and 0555 to" \
+		"all directories in directory on <path> [default <path>: \".\"];"
+	echo -e "\tunlock <path>  - set permissions 0777 to all files and" \
+		"directories in directory on <path> [default <path>: \".\"];"
+	echo -e "\tlist <path>    - show list of all files and directories in" \
+		"directory on <path> [default <path>: \".\"]."
 }
 
 function ShowError() {
 	local -r message=$1
 
-	echo "Error! $message"
+	echo "Error: $message."
 	echo ""
 
 	ShowHelp
@@ -27,24 +32,50 @@ function ShowError() {
 	exit 1
 }
 
-readonly command=$1
-if [[ $command != "lock" && $command != "unlock" ]]
-then
-	ShowError "Invalid command."
-fi
+function GetPath {
+	local path="$1"
+	if [[ -z "$path" ]]
+	then
+		path="."
+	fi
 
-readonly path="$2"
-if [[ -z "$path" ]]
-then
-	ShowError "Empty path."
-fi
+	echo "$path"
+}
 
-case $command in
-	lock)
-		find "$path" -type d -exec chmod 0555 {} \;
-		find "$path" -type f -exec chmod 0444 {} \;
-		;;
-	unlock)
-		find "$path" -exec chmod 0777 {} \;
-		;;
-esac
+function ProcessCommand {
+	local -r command="$1"
+	case $command in
+		-h|--help)
+			ShowHelp
+			exit
+
+			;;
+		*)
+			local -r path=`GetPath "$2"`
+			case $command in
+				lock)
+					find "$path" -type d -exec chmod 0555 {} \;
+					find "$path" -type f -exec chmod 0444 {} \;
+					;;
+				unlock)
+					find "$path" -exec chmod 0777 {} \;
+					;;
+				list)
+					find "$path"
+					;;
+				*)
+					if [[ -z "$command" ]]
+					then
+						ShowError "command should be specified"
+					else
+						ShowError "invalid command \"$command\""
+					fi
+
+					;;
+			esac
+
+			;;
+	esac
+}
+
+ProcessCommand "$@"
