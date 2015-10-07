8e540cceb51cdd41162a19ea6043c473bbe9ede9
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-10-07 04:13:09 +0300

    Issue #115: add client-side saving.

--- protected/views/day/update.php
+++ protected/views/day/update.php
@@ -17,6 +17,10 @@
 		CClientScript::POS_HEAD
 	);
 	Yii::app()->getClientScript()->registerScriptFile(
+		CHtml::asset('scripts/close_dialog.js'),
+		CClientScript::POS_HEAD
+	);
+	Yii::app()->getClientScript()->registerScriptFile(
 		CHtml::asset('scripts/day_editor.js'),
 		CClientScript::POS_HEAD
 	);
@@ -25,6 +29,16 @@
 ?>
 
 <header class = "page-header clearfix header-with-button">
+	<button
+		class = "btn btn-default pull-right close-button"
+		title = "Закрыть"
+		data-date = "<?= $date ?>"
+		data-my-date = "<?= $my_date ?>"
+		data-view-url = "<?=
+			$this->createUrl('day/view', array('date' => $raw_date))
+		?>">
+		<span class = "glyphicon glyphicon-remove"></span>
+	</button>
 	<button
 		class = "btn btn-primary pull-right save-day-button"
 		title = "Сохранить"
@@ -68,7 +82,9 @@
 </header>
 
 <?= CHtml::beginForm(
-	$this->createUrl('day/update', array('date' => $raw_date))
+	$this->createUrl('day/update', array('date' => $raw_date)),
+	'post',
+	array('class' => 'day-form')
 ) ?>
 	<div class = "form-group">
 		<?= CHtml::label('Описание пунктов', 'points_description') ?>
@@ -77,3 +93,48 @@
 		?></div>
 	</div>
 <?= CHtml::endForm() ?>
+
+<div class = "modal close-dialog">
+	<div class = "modal-dialog">
+		<div class = "modal-content">
+			<div class = "modal-header">
+				<button
+					class = "close"
+					type = "button"
+					data-dismiss = "modal"
+					aria-hidden = "true">
+					&times;
+				</button>
+				<h4 class = "modal-title">
+					<span class = "glyphicon glyphicon-warning-sign"></span>
+					Внимание!
+				</h4>
+			</div>
+
+			<div class = "modal-body">
+				<p>
+					День <time class = "day-date"></time> не сохранён.
+					Закрытие редактора может привести к потере последних
+					изменений. Так что ты хочешь сделать?
+				</p>
+			</div>
+
+			<div class = "modal-footer">
+				<button type = "button" class = "btn btn-primary save-button">
+					<span class = "glyphicon glyphicon-floppy-disk"></span>
+					Сохранить и закрыть
+				</button>
+				<button type = "button" class = "btn btn-danger close-button">
+					<span class = "glyphicon glyphicon-remove"></span>
+					Закрыть
+				</button>
+				<button
+					class = "btn btn-default"
+					type = "button"
+					data-dismiss = "modal">
+					Отмена
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
--- scripts/day_editor.js
+++ scripts/day_editor.js
@@ -5,6 +5,39 @@ $(document).ready(
 		day_editor.setShowInvisibles(true);
 		day_editor.setShowPrintMargin(false);
 
+		var saved_flag_container = $('.saved-flag');
+		var saved_flag_icon = $('span', saved_flag_container);
+		var is_saved = true;
+		var SetSavedFlag = function(saved) {
+			is_saved = saved;
+
+			if (saved) {
+				saved_flag_container
+					.addClass('label-success')
+					.removeClass('label-danger');
+				saved_flag_icon
+					.addClass('glyphicon-floppy-saved')
+					.removeClass('glyphicon-floppy-remove');
+
+				var last_symbol = document.title.slice(-1);
+				if (last_symbol.length && last_symbol == '*') {
+					document.title = document.title.slice(0, -1);
+				}
+			} else {
+				saved_flag_container
+					.addClass('label-danger')
+					.removeClass('label-success');
+				saved_flag_icon
+					.addClass('glyphicon-floppy-remove')
+					.removeClass('glyphicon-floppy-saved');
+
+				var last_symbol = document.title.slice(-1);
+				if (last_symbol.length && last_symbol != '*') {
+					document.title += '*';
+				}
+			}
+		};
+
 		var number_of_points_view = $('.number-of-points-view');
 		var SetNumberOfPoints = function() {
 			var points_description = day_editor.getValue();
@@ -27,7 +60,94 @@ $(document).ready(
 		day_editor.on(
 			'change',
 			function() {
+				SetSavedFlag(false);
 				SetNumberOfPoints();
+			}
+		);
+
+		var save_button = $('.save-day-button');
+		var save_url = save_button.data('save-url');
+		var processing_animation_image = $('img', save_button);
+		var save_icon = $('span', save_button);
+		var day_editor_container = $(day_editor.container);
+		var FinishAnimation = function() {
+			save_button.prop('disabled', false);
+			processing_animation_image.hide();
+			save_icon.show();
+			day_editor_container.removeClass('wait');
+		};
+		var SaveViaAjax = function(callback) {
+			save_button.prop('disabled', true);
+			processing_animation_image.show();
+			save_icon.hide();
+			day_editor_container.addClass('wait');
+
+			var data = $.extend(
+				{'points_description': day_editor.getValue()},
+				CSRF_TOKEN
+			);
+			$.post(
+				save_url,
+				data,
+				function() {
+					SetSavedFlag(true);
+					FinishAnimation();
+
+					if (callback) {
+						callback();
+					}
+				}
+			).fail(
+				function(xhr, text_status) {
+					FinishAnimation();
+					AjaxErrorDialog.handler(xhr, text_status);
+				}
+			);
+		};
+		save_button.click(SaveViaAjax);
+		$(window).keydown(
+			function(event) {
+				if (
+					(event.ctrlKey || event.metaKey)
+					&& String.fromCharCode(event.which).toLowerCase() == 's'
+				) {
+					event.preventDefault();
+					SaveViaAjax();
+				}
+			}
+		);
+
+		var close_button = $('.close-button');
+		var day_date = close_button.data('date');
+		var day_my_date = close_button.data('my-date');
+		var view_url = close_button.data('view-url');
+		var CloseDayEditor = function() {
+			location.href = view_url;
+		};
+		close_button.click(
+			function() {
+				if (!is_saved) {
+					CloseDialog.show(
+						day_my_date,
+						day_date,
+						function() {
+							CloseDialog.hide();
+							SaveViaAjax(CloseDayEditor);
+						},
+						CloseDayEditor
+					);
+				} else {
+					CloseDayEditor();
+				}
+			}
+		);
+
+		var form = $('.day-form');
+		form.submit(
+			function() {
+				event.preventDefault();
+				event.stopPropagation();
+				return false;
 			}
 		);
 	}
