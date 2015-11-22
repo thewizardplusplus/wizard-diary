e8c141c7bbabdf7b7482d949dd4df3e56cbf366c
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-11-22 16:15:24 +0300

    Issue #135: calculate point hierarchy.

--- protected/controllers/DayController.php
+++ protected/controllers/DayController.php
@@ -136,6 +136,8 @@ class DayController extends CController {
 		$points_description = $this->prepareImport($points);
 		$encoded_date = CHtml::encode($date);
 		$stats = $this->getStats($date);
+		$point_hierarchy = $this->getPointHierarchy();
+		Yii::log(print_r($point_hierarchy, true), 'info');
 
 		$this->render(
 			'update',
@@ -196,6 +198,59 @@ class DayController extends CController {
 		}
 
 		return $row;
+	}
+
+	private function getPointHierarchy() {
+		$points = Point::model()->findAll(
+			array(
+				'select' => array('text'),
+				'condition' => '`daily` = FALSE AND LENGTH(TRIM(`text`)) > 0'
+			)
+		);
+
+		$hierarchy = array();
+		$tails = array();
+		foreach ($points as $point) {
+			$parts = array_map('trim', explode(',', $point->text));
+			$number_of_parts = count($parts);
+			if ($number_of_parts > 0) {
+				if (!array_key_exists($parts[0], $hierarchy)) {
+					$hierarchy[$parts[0]] = array();
+				}
+			}
+			if ($number_of_parts > 1) {
+				if (!in_array($parts[1], $hierarchy[$parts[0]])) {
+					$hierarchy[$parts[0]][] = $parts[1];
+				}
+			}
+			if ($number_of_parts > 2) {
+				$tail = implode(', ', array_slice($parts, 2));
+				$result = preg_match(
+					'/[A-Z]/i',
+					$tail,
+					$matches,
+					PREG_OFFSET_CAPTURE
+				);
+				if ($result == 1) {
+					$tail = trim(substr($tail, 0, $matches[0][1]));
+				}
+
+				$tails[] = $tail;
+			}
+		}
+
+		$new_hierarchy = array();
+		foreach ($hierarchy as $level_1 => $level_2_list) {
+			sort($level_2_list, SORT_STRING);
+			$new_hierarchy[$level_1] = $level_2_list;
+		}
+		ksort($new_hierarchy, SORT_STRING);
+		$hierarchy = $new_hierarchy;
+
+		$tails = array_unique($tails);
+		sort($tails);
+
+		return array('hierarchy' => $hierarchy, 'tails' => $tails);
 	}
 
 	private function prepareImport($points) {
