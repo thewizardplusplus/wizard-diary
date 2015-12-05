41f75552a17c282b776638558cd6e90abb3459af
thewizardplusplus <thewizardplusplus@yandex.ru>
2015-12-05 20:12:12 +0300

    Issue #183: add timestamps to diff view.

--- protected/views/backup/diff.php
+++ protected/views/backup/diff.php
@@ -2,6 +2,8 @@
 	/**
 	 * @var BackupController $this
 	 * @var string $diff_representation
+	 * @var string $previous_file_timestamp
+	 * @var string $file_timestamp
 	 */
 
 	Yii::app()->getClientScript()->registerPackage('ace');
@@ -11,11 +13,26 @@
 		CClientScript::POS_HEAD
 	);
 
-	$this->pageTitle = Yii::app()->name . ' - Бекапы: изменения';
+	$this->pageTitle =
+		Yii::app()->name
+		. ' - Бекапы:'
+		. (is_null($file_timestamp) ? ' текущие ' : ' ')
+		. 'изменения';
 ?>
 
 <header class = "page-header">
-	<h4>Бекапы: изменения</h4>
+	<h4>Бекапы: <?= is_null($file_timestamp) ? 'текущие' : '' ?> изменения</h4>
 </header>
+
+<p>
+	Изменения с момента
+	<strong><time><?= $previous_file_timestamp ?></time></strong>
+	к
+	<?php if (!is_null($file_timestamp)) { ?>
+		моменту <strong><time><?= $file_timestamp ?></time></strong>:
+	<?php } else { ?>
+		текущему моменту:
+	<?php } ?>
+</p>
 
 <div id = "diff-viewer"><?= CHtml::encode($diff_representation) ?></div>
