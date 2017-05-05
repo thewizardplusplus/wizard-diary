5d6815e14e56fac49834254bbd9a6e54edb41fa0
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-05-05 03:14:43 +0300

    Issue #252: add the start option parsing

--- tools/git_importer.py
+++ tools/git_importer.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3.5
 
 import argparse
+import datetime
+
+import parsedatetime
+import tzlocal
+
+def parse_timestamp(value):
+    try:
+        timestamp = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S%z')
+    except Exception:
+        timestamp, status = parsedatetime.Calendar().parseDT(value)
+        if status == 0:
+            raise argparse.ArgumentTypeError(
+                'timestamp {} is incorrect'.format(value),
+            )
+
+        timestamp = tzlocal.get_localzone().localize(timestamp, is_dst=None)
+
+    return timestamp
 
 def parse_options():
     parser = argparse.ArgumentParser(
@@ -11,6 +29,14 @@ def parse_options():
         '--repo',
         default='.',
         help='path to the repository',
+    )
+    parser.add_argument(
+        '-s',
+        '--start',
+        type=parse_timestamp,
+        required=True,
+        help='a start timestamp of the repository log ' \
+            + 'in ISO 8601 or human-readable formats',
     )
 
     return parser.parse_args()
