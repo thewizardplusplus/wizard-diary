1362022401a8e534b463f3fe29f56416058f1d9a
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-05-08 00:34:15 +0300

    Issue #254: correct a markup of the import button

--- protected/views/point/import.php
+++ protected/views/point/import.php
@@ -6,21 +6,12 @@
 	$this->pageTitle = Yii::app()->name . ' - Импорт пунктов';
 ?>
 
-<header class = "page-header clearfix header-with-button">
-	<?= CHtml::beginForm(
-		$this->createUrl('point/import'),
-		'post',
-		array('class' => 'form-inline pull-right import-points-form')
-	) ?>
-		<?= CHtml::htmlButton(
-			'<span class = "glyphicon glyphicon-import"></span> Импортировать',
-			array(
-				'class' => 'btn btn-primary import-button',
-				'type' => 'submit',
-				'disabled' => 'disabled',
-			)
-		) ?>
-	<?= CHtml::endForm() ?>
+<header class="page-header clearfix header-with-button">
+	<button
+		class="btn btn-primary pull-right import-button"
+		title="Импортировать">
+		<span class="glyphicon glyphicon-import"></span> Импортировать
+	</button>
 
 	<h4>Импорт пунктов</h4>
 </header>
