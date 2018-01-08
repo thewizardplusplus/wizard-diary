5e4fdeaaff4718928799c5f2f3f5965e978b6f2f
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-08 20:47:42 +0300

    Up the version

--- protected/config/Constants.php
+++ protected/config/Constants.php
@@ -6,7 +6,7 @@ class Constants extends AccessConstants {
 	const DEBUG = false;
 	const TRACE_LEVEL = 5;
 	const APP_NAME = 'Wizard Diary';
-	const APP_VERSION = 'v7.1';
+	const APP_VERSION = 'v7.2';
 	const DATABASE_TABLE_PREFIX = 'diary_';
 	const DEFAULT_PASSWORD = 'admin';
 	const COPYRIGHT_START_YEAR = 2014;
