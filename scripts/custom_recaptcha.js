44b6bae97eb61bea42f1b820e1d2885f538f6d6b
thewizardplusplus <thewizardplusplus@yandex.ru>
2018-01-09 04:33:57 +0300

    Issue #257: fix the script of the CAPTCHA view

--- scripts/custom_recaptcha.js
+++ scripts/custom_recaptcha.js
@@ -1,32 +1,3 @@
-var RecaptchaOptions = {
-	theme: 'custom',
-	lang: 'ru',
-	custom_theme_widget: 'recaptcha_widget'
-};
-
-$(document).ready(
-	function() {
-		var RequestAnimationFrame =
-			requestAnimationFrame
-				|| mozRequestAnimationFrame
-				|| webkitRequestAnimationFrame
-				|| function(callback) {
-				setTimeout(callback, 1000 / 60);
-			};
-		var CorrectImageStyle = function() {
-			RequestAnimationFrame(CorrectImageStyle);
-
-			var image_container = $('#recaptcha_image');
-			image_container.width('auto');
-			image_container.height('auto');
-
-			var image = $('img', image_container);
-			if (image.length && !image.hasClass('img-thumbnail')) {
-				image.addClass('img-thumbnail');
-			}
-		};
-
-		CorrectImageStyle();
-		$('.recaptcha-refresh').click(Recaptcha.reload);
-	}
-);
+function process_recaptcha_response(response) {
+	$('#LoginForm_verify_code').val(response);
+}
