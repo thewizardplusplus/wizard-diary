7643ee592af12a21f8159b6abf2ebb348422356e
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-04-29 20:23:08 +0500

    feat(tools/loop-importer): :sparkles: Add missing habits in the `processing.group_habit_repetitions_by_date()` function

--- tools/loop-importer/loop_importer/processing.py
+++ tools/loop-importer/loop_importer/processing.py
@@ -4,18 +4,34 @@ from typing import List
 from . import models
 
 def group_habit_repetitions_by_date(habits: List[models.Habit]) -> models.HabitRepetitionsByDate:
-    habit_repetitions_by_date = defaultdict(list)
+    habit_repetitions_by_id_and_date = defaultdict(dict)
     for habit in habits:
         for repetition in habit.repetitions:
-            habit_repetitions_by_date[repetition.date].append(models.HabitRepetition(
+            habit_repetitions_by_id_and_date[repetition.date][habit.id] = models.HabitRepetition(
                 habit_id=habit.id,
                 habit_name=habit.name,
                 habit_position=habit.position,
                 is_habit_archived=habit.is_archived,
                 value=repetition.value,
-            ))
+            )
 
-    return dict(habit_repetitions_by_date)
+    for habit_repetitions_by_id in habit_repetitions_by_id_and_date.values():
+        for habit in habits:
+            if habit.id in habit_repetitions_by_id:
+                continue
+
+            habit_repetitions_by_id[habit.id] = models.HabitRepetition(
+                habit_id=habit.id,
+                habit_name=habit.name,
+                habit_position=habit.position,
+                is_habit_archived=habit.is_archived,
+                value=models.RepetitionValue.NO,
+            )
+
+    return {
+        date: list(habit_repetitions_by_id.values())
+        for date, habit_repetitions_by_id in habit_repetitions_by_id_and_date.items()
+    }
 
 def format_habit_repetitions_by_date_to_markdown(
     habit_repetitions_by_date: models.HabitRepetitionsByDate,
