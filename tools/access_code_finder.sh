f34f37100c1c862cdedaf08d71ea74bc17fb8337
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-05-19 23:22:25 +0500

    refactor(tools): :recycle: Simplify the code of the `access_code_finder.sh` tool

--- tools/access_code_finder.sh
+++ tools/access_code_finder.sh
@@ -1,43 +1,11 @@
 #!/usr/bin/env bash
 
-function ShowHelp() {
-	local -r script_name=`basename $0`
+declare -r script_path="$(dirname "$0")"
 
-	echo "Using:"
-	echo -e "\t$script_name [-h | --help]"
-	echo ""
-	echo "Options:"
-	echo -e "\t-h, --help  - show help."
-}
-
-function ProcessOptions() {
-	local -r option="$1"
-
-	case "$option" in
-		-h|--help)
-			ShowHelp
-			exit
-
-			;;
-	esac
-}
-
-function FindAccessCode() {
-	local -r script_path=`dirname $0`
-
+declare -r access_code="$(
 	cat "$script_path/../protected/runtime/application.log" \
 		| grep "access code" \
 		| tail -1 \
 		| awk '{print $NF}'
-}
-
-function OutputToStdout() {
-	local -r message="$1"
-
-	echo "$message"
-}
-
-ProcessOptions "$@"
-
-readonly access_code=`FindAccessCode`
-OutputToStdout "$access_code"
+)"
+echo "info: detected the access code \"$access_code\""
