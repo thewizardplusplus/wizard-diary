6f63f71c4ff514dba0a1d467ab31b792fcf0e3eb
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-10-07 06:52:11 +0500

    feat(scripts): :sparkles: Update the day stats on day finishing
    Fix #274

--- scripts/finishing.js
+++ scripts/finishing.js
@@ -6,7 +6,46 @@ $(document).ready(function () {
   var stats_url = finishing_button.data("stats-url");
   var processing_animation_image = $("img", finishing_button);
   var finishing_icon = $("span", finishing_button);
+  var day_completed_flag = $(".day-completed-flag");
+  var day_completed_inner_flag = $("span.glyphicon", day_completed_flag);
   var point_list = $("#point-list");
+  var UpdateDayCompletedFlag = function (data) {
+    if (data.completed == "1") {
+      day_completed_flag
+        .attr("title", "Завершён")
+        .removeClass("label-primary")
+        .addClass("label-success");
+      day_completed_inner_flag
+        .removeClass("glyphicon-unchecked")
+        .addClass("glyphicon-check");
+    } else {
+      day_completed_flag
+        .attr("title", "Не завершён")
+        .removeClass("label-success")
+        .addClass("label-primary");
+      day_completed_inner_flag
+        .removeClass("glyphicon-check")
+        .addClass("glyphicon-unchecked");
+    }
+  };
+  var UpdateDaySatisfiedView = function (data) {
+    var text = "&mdash;";
+    if (data.satisfied != -1) {
+      text = data.satisfied + "%";
+    }
+
+    $(".day-satisfied-view").html(text);
+  };
+  var UpdateDayStats = function () {
+    $.get(
+      stats_url,
+      function (data) {
+        UpdateDayCompletedFlag(data);
+        UpdateDaySatisfiedView(data);
+      },
+      "json"
+    ).fail(AjaxErrorDialog.handler);
+  };
 
   finishing_button.click(function () {
     FinishingDialog.show(function () {
@@ -23,6 +62,7 @@ $(document).ready(function () {
           point_list.yiiGridView("update", {
             url: location.pathname + location.search + location.hash,
           });
+          UpdateDayStats();
 
           processing_animation_image.hide();
           finishing_icon.show();
