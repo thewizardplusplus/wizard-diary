58eb67fc1741851b0715293627f7ca582a5f5050
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-10-07 05:10:36 +0500

    feat(protected/views/day): :sparkles: Add the finishing button
    Issue #274

---
+++ scripts/finishing.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  var finishing_button = $(".finishing-button");
+  var finishing_url = finishing_button.data("finishing-url");
+  var processing_animation_image = $("img", finishing_button);
+  var finishing_icon = $("span", finishing_button);
+  var point_list = $("#point-list");
+
+  finishing_button.click(function () {
+    FinishingDialog.show(function () {
+      finishing_button.attr("disabled", "disabled");
+
+      processing_animation_image.show();
+      finishing_icon.hide();
+
+      point_list.yiiGridView("update", {
+        type: "POST",
+        url: finishing_url,
+        data: CSRF_TOKEN,
+        success: function (data) {
+          point_list.yiiGridView("update", {
+            url: location.pathname + location.search + location.hash,
+          });
+
+          processing_animation_image.hide();
+          finishing_icon.show();
+        },
+      });
+
+      FinishingDialog.hide();
+    });
+  });
+});
